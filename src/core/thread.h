8d8c8480631d28b8e01ba9a015fbbb2d8303dfd8
FreezB11 <yashrajrao4@gmail.com>
2026-06-14 20:47:15 +0530

    a plan ahead

---
+++ src/core/thread.h
@@ -0,0 +1,22 @@
+#include "../arch/x86_64/context.h"
+#include "stddef.h"
+
+enum State{
+    READY,
+    RUNNING,
+    BLOCKED,
+    DEAD
+};
+
+typedef
+struct thread_t{
+    context_t ctx;
+    State state;
+    void* stack;
+    size_t stack_size;
+    uint64_t tid;
+    // we can give it some stack i assume
+}thread_t;
+
+thread_t* t_create(void (*fn)(void*), void* arg);
+size_t t_join(thread_t* t);

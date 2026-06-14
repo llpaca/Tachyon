34cc21dcb923b4b9fcc08993c43fcc7ca8c6ebaa
FreezB11 <yashrajrao4@gmail.com>
2026-06-14 20:46:16 +0530

    @todo this is how i want it to be

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "include/tachyon.h"
+
+void worker(){
+
+}
+
+int main(){
+
+    tachyon_init();
+    int n = 10;
+    //lets say we want n-gthreads for a worker
+    tachyon_spool(n, worker, (void*)"this is a test");
+
+    //for single thread
+    tachyon_thread(worker, (void*)"single thread");
+
+    return 0;
+}
